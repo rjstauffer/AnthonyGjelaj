614a776ae23cff37a384a6fa40f03cfc4ba4c075
rjstauffer <rileyjstauffer@gmail.com>
2024-04-16 10:04:28 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadshotComponent } from './headshot/headshot.component';
+import { NameComponent } from './name/name.component';
+import { ColumnsComponent } from './columns/columns.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadshotComponent,
+    NameComponent,
+    ColumnsComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/headshot/headshot.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headshot',
+  templateUrl: './headshot.component.html',
+  styleUrls: ['./headshot.component.css']
+})
+export class HeadshotComponent {
+
+}
